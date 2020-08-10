b9d062ada65083da02a2c95c9a14c445ceeef7ad
JessicaHernande <65617310+JessicaHernande@users.noreply.github.com>
2020-08-10 11:33:43 -0500

    Update loginU.php

--- PASTELERIA2.1/BBDD/loginU.php
+++ PASTELERIA2.1/BBDD/loginU.php
@@ -2,14 +2,13 @@
 $usuario=$_POST['usuario'];
 $clave=$_POST['clave'];
 
-$conn = new mysqli("localhost", "root", "", "dblogin");
+$conn = new mysqli("localhost", "root", "", "bdprueba");
 $consulta="SELECT * FROM usuarios WHERE usuario='$usuario' and clave='$clave'";
 $resultado=mysqli_query($conexion, $consulta);
 
 $filas=mysqli_num_rows($resultado);
 if($filas>0){
-  $sLang = lang();
-  header("Location: Index.php?lang=$sLang");
+  header("Location: Index.php");
 }
 else {
   echo "Error en la autenticacion";
